a1128b11cc7f14f938adc2fd23737d9d33e5fede
owefsad <dongzhiyong@secnium.cn>
2022-01-14 14:30:52 +0800

    first commit

---
+++ src/main/java/club/owefsad/maven/MavenRepoDownloadApplication.java
@@ -0,0 +1,13 @@
+package club.owefsad.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenRepoDownloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenRepoDownloadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/owefsad/maven/MavenRepoDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package club.owefsad.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenRepoDownloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
